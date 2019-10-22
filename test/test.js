7bda3bddfc3fb2a8c96cb5c2f688f594963045eb
David Thomason <dtom90@users.noreply.github.com>
2019-10-22 17:35:07 -0400

    Added another test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
+const {describe, it} = require('mocha')
 const expect = require('chai').expect
+
 const colorManager = require('../index')
 
 describe('color-manager', function() {
@@ -6,6 +8,18 @@ describe('color-manager', function() {
     it('should import a function named getRandomColor', function() {
       expect(colorManager).to.have.property('getRandomColor')
       expect(colorManager.getRandomColor).to.be.a('function')
+    })
+    
+    it('should generate a different random color each time', () => {
+      
+      const color1 = colorManager.getRandomColor()
+      expect(color1).to.be.a('string')
+  
+      const color2 = colorManager.getRandomColor()
+      expect(color2).to.be.a('string')
+  
+      expect(color1).not.to.equal(color2)
+      
     })
   })
 })
