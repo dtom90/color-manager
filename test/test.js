11fc72638e3a67f05ccd5e8b17dfc77428e85aad
David Thomason <dtom90@users.noreply.github.com>
2019-10-28 09:03:26 -0400

    Converted to class

--- test/test.js
+++ test/test.js
@@ -1,25 +1,40 @@
 const {describe, it} = require('mocha')
 const expect = require('chai').expect
 
-const colorManager = require('../index')
+const ColorManager = require('../index')
 
-describe('color-manager', function() {
-  describe('getRandomColor', function() {
-    it('should import a function named getRandomColor', function() {
-      expect(colorManager).to.have.property('getRandomColor')
-      expect(colorManager.getRandomColor).to.be.a('function')
-    })
+describe('ColorManager', () => {
+  it('should import a class named ColorManager', () => {
+    expect(ColorManager).to.be.a('function')
+  })
+  
+  it('should have a method named getRandomColor', () => {
+    expect(ColorManager.prototype).to.have.property('getRandomColor')
+    expect(ColorManager.prototype.getRandomColor).to.be.a('function')
+  })
+  
+  it('should not have any other methods', () => {
+    expect(ColorManager.prototype).not.to.have.property('generateColor')
+    expect(ColorManager.prototype).not.to.have.property('getMinDist')
+    expect(ColorManager.prototype).not.to.have.property('tripconstValues')
+    expect(ColorManager.prototype).not.to.have.property('colorLightness')
+  })
+  
+  it('should generate a different random color each time', () => {
     
-    it('should generate a different random color each time', () => {
-      
-      const color1 = colorManager.getRandomColor()
-      expect(color1).to.be.a('string')
-  
-      const color2 = colorManager.getRandomColor()
-      expect(color2).to.be.a('string')
-  
-      expect(color1).not.to.equal(color2)
-      
-    })
+    const myColorManager = new ColorManager()
+    
+    expect(myColorManager.colors).to.deep.equal([])
+    
+    const color1 = myColorManager.getRandomColor()
+    expect(color1).to.be.a('string')
+    
+    const color2 = myColorManager.getRandomColor()
+    expect(color2).to.be.a('string')
+    
+    expect(color1).not.to.equal(color2)
+
+    expect(myColorManager.colors).to.deep.equal([color1, color2])
+    
   })
 })
